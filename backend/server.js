77b1df899a352d4fbdc989af6064a13a4b9baac9
M A Reno <benedictreno47@gmail.com>
2024-12-04 17:28:04 +0530

    Added my project

---
+++ backend/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employee_db"
+});
+
+db.connect((error)=> {
+    if (error) {
+        console.log("Error connecting to database");
+    }
+    else {
+        app.listen(8000, () => {
+            console.log("Server connected to port 8000 and DB connected");
+        });
+    }
+})
+
+app.post("/register",(req,res)=> {
+    try {
+        const {name, id, email, phone, department, doj, role} = req.body;
+        const getidquery = "SELECT * FROM employee WHERE id = ? OR email = ?";
+        db.query(getidquery,[id,email],(err,result)=>{
+            if (err) {
+                console.log("Error checking existing records"+err);
+                return res.status(404).send({ok: false, message: "Error getting employee details"});
+            }
+            else {
+                if (result.length > 0) {
+                    const existingEmp = result[0];
+                    if (existingEmp.id == id) {
+                        return res.status(404).send({ok: false, message: "Employee id already exist"});
+                    }
+                    else if (existingEmp.email == email) {
+                        return res.status(404).send({ok: false, message: "Employee email already exist"});
+                    }
+                }
+            }
+
+            const insertquery = "INSERT INTO employee (name, id, email, phone, department, doj, role) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            db.query(insertquery,[name, id, email, phone, department, doj, role],(err,result)=>{
+                if (err) {
+                    console.log("Error inserting employee records"+err);
+                    return res.status(404).send({ok: false, message: "Error inserting employee details"});
+                }
+                else {
+                    res.status(200).json({ok: true, message: "student register successfully"});
+                }
+            })
+        })
+    } catch (error) {
+        return res.status(500).send({ok: false, message: "Error registering employee data"})
+    }
+})
